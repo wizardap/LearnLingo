5404c3ad7bab811c2895e3789b1e5af56b51f32f
vuniem131104 <124224840+vuniem131104@users.noreply.github.com>
2023-09-24 14:53:05 +0700

    updateInterface

--- src/main/java/com/application/learnlingo/DictionaryController.java
+++ src/main/java/com/application/learnlingo/DictionaryController.java
@@ -1,14 +1,33 @@
 package com.application.learnlingo;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
 public class DictionaryController {
     @FXML
-    private Label welcomeText;
-
+    private Button change;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ImageView british;
+    @FXML
+    private ImageView vn;
+    @FXML
+    private HBox changeDictionary;
+    @FXML
+    private boolean isUKFlagVisible = true;
+    @FXML
+    public void changeMode() {
+        change.setOnAction(event -> {
+            if (isUKFlagVisible) {
+                changeDictionary.getChildren().removeAll(british, vn, change);
+                changeDictionary.getChildren().addAll(vn, change, british);
+            } else {
+                changeDictionary.getChildren().removeAll(vn, british, change);
+                changeDictionary.getChildren().addAll(british, change, vn);
+            }
+            isUKFlagVisible = !isUKFlagVisible;
+        });
     }
 }
