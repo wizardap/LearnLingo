c53bfcc887193a8a9b9249bffac1deaded7fcee8
WizardAP <quanvi03@gmail.com>
2023-09-24 09:57:41 +0700

    Add JavaFX framework

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.application.learnlingo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.application.learnlingo to javafx.fxml;
+    exports com.application.learnlingo;
+}
