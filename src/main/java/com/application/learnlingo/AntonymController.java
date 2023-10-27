8397d6f673ff5408ffada7bf4ee3bd4e63cdb98c
tuan15032 <nguyenducanhtuan22092004@gmail.com>
2023-10-27 19:07:58 +0700

    update syn and ant

--- src/main/java/com/application/learnlingo/AntonymController.java
+++ src/main/java/com/application/learnlingo/AntonymController.java
@@ -15,86 +15,22 @@ import java.util.ResourceBundle;
 
 
 public class AntonymController extends SynonymAndAntonym {
-
-    public Button a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15;
-    public Button[] buttons;
-
-
+    @Override
+    public void handleMouseClicked(MouseEvent mouseEvent) {
+        super.handleMouseClicked(mouseEvent, "ant");
+    }
+    @Override
+    public void buttonClicked(MouseEvent mouseEvent){
+        super.buttonClicked(mouseEvent, "ant");
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         super.initialize(url, resourceBundle);
 
-        buttons = new Button[]{a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15};
+        buttons = new Button[]{b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15};
 
         synonym.setOnAction(e -> AnimationChangeScene.handleButtonClick("FindSynonym.fxml", container));
     }
 
-    public List<String> setListSyn(String s) {
-        List<String> result = new ArrayList<>();
-        try {
-            String apiUrl = "https://api.datamuse.com/words?rel_ant=" + s.replaceAll(" ", "+");
 
-            URL url = new URL(apiUrl);
-
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            StringBuilder response = new StringBuilder();
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
-            }
-
-            reader.close();
-
-            connection.disconnect();
-
-            String[] words = response.toString().split("(?<=\\}),");
-            for (String word : words) {
-                String[] wordParts = word.split(":");
-                if (wordParts.length >= 2) {
-                    String cleanWord = wordParts[1].replaceAll("\"", "");
-                    cleanWord = cleanWord.split(",")[0];
-                    result.add(cleanWord);
-                }
-
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-
-    @Override
-    public void handleMouseClicked(MouseEvent mouseEvent) {
-        String selectedWord = listWords.getSelectionModel().getSelectedItem();
-        if (selectedWord != null) {
-            try {
-                FileReader fr = new FileReader(historyTxt);
-                BufferedReader input = new BufferedReader(fr);
-                buttons = new Button[]{a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15};
-                List<String> result = this.setListSyn(selectedWord);
-                if (result.size() == 0) {
-                    buttons[0].setText("There is no antonym for this word.");
-                    for (int i = 1; i < 15; i++) {
-                        buttons[i].setVisible(false);
-                    }
-                } else {
-                    for (int i = 0; i < 15; i++) {
-                        if (i < result.size()) {
-                            buttons[i].setVisible(true);
-                            buttons[i].setText(result.get(i));
-                        } else {
-                            buttons[i].setVisible(false);
-                        }
-                    }
-                }
-                input.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
--- src/main/java/com/application/learnlingo/SynonymAndAntonym.java
+++ src/main/java/com/application/learnlingo/SynonymAndAntonym.java
@@ -1,18 +1,49 @@
 package com.application.learnlingo;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 
+import javax.swing.*;
 import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
-public class SynonymAndAntonym extends GeneralController {
+public abstract class SynonymAndAntonym extends GeneralController {
+    public Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15;
+    public Button[] buttons;
 
+    public void setBut(String s, Button x){
+        buttons = new Button[]{b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15};
+        List<String> result = this.setList(x.getText(), s);
+        if (result.isEmpty()) {
+            if (Objects.equals(s, "syn")) buttons[0].setText("There is no synonym for this word.");
+            else buttons[0].setText("There is no antonym for this word.");
+            for (int i = 1; i < 15; i++) {
+                buttons[i].setVisible(false);
+            }
+        } else {
+            for (int i = 0; i < 15; i++) {
+                if (i < result.size()) {
+                    buttons[i].setVisible(true);
+                    buttons[i].setText(result.get(i));
+                } else {
+                    buttons[i].setVisible(false);
+                }
+            }
+        }
+    }
+    public void buttonClicked(MouseEvent mouseEvent,String s) {
+        Button clickedButton = (Button) mouseEvent.getSource();
+        setBut(s, clickedButton);
+    }
     public void changeMode() {
         if (isUKFlagVisible) {
             changeDictionary.getChildren().removeAll(british, vn, change);
@@ -32,8 +63,74 @@ public class SynonymAndAntonym extends GeneralController {
         isUKFlagVisible = !isUKFlagVisible;
     }
 
-    public void handleMouseClicked(MouseEvent mouseEvent) {
 
+    public List<String> setList(String s, String a) {
+        List<String> result = new ArrayList<>();
+        try {
+            String apiUrl = "https://api.datamuse.com/words?rel_" + a + "=" + s.replaceAll(" ", "+");
+
+            URL url = new URL(apiUrl);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+
+            reader.close();
+
+            connection.disconnect();
+
+            String[] words = response.toString().split("(?<=\\}),");
+            for (String word : words) {
+                String[] wordParts = word.split(":");
+                if (wordParts.length >= 2) {
+                    String cleanWord = wordParts[1].replaceAll("\"", "");
+                    cleanWord = cleanWord.split(",")[0];
+                    result.add(cleanWord);
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public void handleMouseClicked(MouseEvent mouseEvent, String s) {
+        String selectedWord = listWords.getSelectionModel().getSelectedItem();
+        if (selectedWord != null) {
+            try {
+                FileReader fr = new FileReader(historyTxt);
+                BufferedReader input = new BufferedReader(fr);
+                buttons = new Button[]{b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15};
+                List<String> result = this.setList(selectedWord, s);
+                if (result.isEmpty()) {
+                    if (Objects.equals(s, "syn")) buttons[0].setText("There is no synonym for this word.");
+                    else buttons[0].setText("There is no antonym for this word.");
+                    for (int i = 1; i < 15; i++) {
+                        buttons[i].setVisible(false);
+                    }
+                } else {
+                    for (int i = 0; i < 15; i++) {
+                        if (i < result.size()) {
+                            buttons[i].setVisible(true);
+                            buttons[i].setText(result.get(i));
+                        } else {
+                            buttons[i].setVisible(false);
+                        }
+                    }
+                }
+                input.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
@@ -43,7 +140,7 @@ public class SynonymAndAntonym extends GeneralController {
         listWords.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         listWords.setCellFactory(param -> new DictionaryController.IconAndFontListCell());
         BackgroundImage backgroundImage = new BackgroundImage(
-                new javafx.scene.image.Image(getClass().getResource("image/bg3.jpg").toString(), 910, 600, false, true),
+                new Image(getClass().getResource("image/bg3.jpg").toString(), 910, 600, false, true),
                 BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
                 BackgroundSize.DEFAULT);
 
@@ -94,5 +191,7 @@ public class SynonymAndAntonym extends GeneralController {
             throw new RuntimeException(e);
         }
     }
+
+    public abstract void buttonClicked(MouseEvent mouseEvent);
 }
 
